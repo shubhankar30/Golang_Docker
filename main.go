bd19d6db4698cf7e0e0d51393db857ed077c9c97
shubhankar <shubhankar@to-on.com>
2019-05-14 13:23:08 +0900

    Working go api server with gin and mysql integration

--- main.go
+++ main.go
@@ -5,10 +5,8 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
-	"html/template"
 	"log"
 	"net/http"
-	"time"
 )
 
 type Welcome struct {
@@ -23,8 +21,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 
 	router := gin.Default()
-	router.Use(static.Serve("/", static.LocalFile("./views", true)))
-
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello from %v", "Gin")
+	})
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "views/index.html", nil)
+	})
 	cnn, err := sql.Open("mysql", "docker:docker@tcp(db:3306)/test_db")
 	if err != nil {
 		log.Fatal(err)
@@ -68,6 +70,6 @@ func main() {
 	// fmt.Println("Listening")
 	// fmt.Println(http.ListenAndServe(":8080", nil))
 
-	router.Run(":3000")
+	router.Run()
 
 }
